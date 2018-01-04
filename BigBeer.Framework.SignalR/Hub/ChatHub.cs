6960a604117e746a1435104c95f21520933cb7a0
吴纪雄 <243174414@qq.com>
2018-01-04 21:02:50 +0800

    SignalR在线通讯

--- BigBeer.Framework.SignalR/Hub/ChatHub.cs
+++ BigBeer.Framework.SignalR/Hub/ChatHub.cs
@@ -4,6 +4,13 @@ namespace BigBeer.Framework.SignalR
 {
     public class ChatHub : Hub
     {
+        /// <summary>
+        /// 自定义发送方法
+        /// ALL后面可以自定义，调用根据自定义的函数名调用
+        /// 整个项目只需要引用一个SignalR类库以及两个SignalR.JS即可
+        /// </summary>
+        /// <param name="name">接收的参数</param>
+        /// <param name="message">接受的信息参数</param>
         public void Send(string name,string message)
         {
             Clients.All.addNewMessageToPage(name, message);
