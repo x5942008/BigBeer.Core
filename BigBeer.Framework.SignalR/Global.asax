a46b896e39ec14445072863027465a90eca76fbe
吴纪雄 <243174414@qq.com>
2018-01-04 20:56:17 +0800

    SignalR在线通讯

---
+++ BigBeer.Framework.SignalR/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BigBeer.Framework.SignalR.MvcApplication" Language="C#" %>
---
+++ BigBeer.Framework.SignalR/Hub/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace BigBeer.Framework.SignalR
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name,string message)
+        {
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
