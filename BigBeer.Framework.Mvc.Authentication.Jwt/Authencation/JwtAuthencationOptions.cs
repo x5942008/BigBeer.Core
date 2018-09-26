bf961ac6001939d2f18d2a466239615739efb4ed
吴纪雄 <549590615@qq.com>
2018-09-26 16:35:09 +0800

    jwt

---
+++ BigBeer.Framework.Mvc.Authentication.Jwt/Authencation/JwtAuthencationOptions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBeer.Framework.Mvc.Authentication.Jwt.Authencation
+{
+    public class JwtAuthencationOptions:JwtBearerOptions
+    {
+        public JwtAuthencationOptions() : base()
+        {
+
+        }
+        /// <summary>
+        /// 加密字符串
+        /// </summary>
+        public string Serect { get; set; }
+
+        public new IList<ISecurityTokenValidator> SecurityTokenValidators => new List<ISecurityTokenValidator>() { new JwtAuthencationCoreSecurityTokenHandler(this) };
+    }
+}
---
+++ BigBeer.Framework.Mvc.Authentication.Jwt/JwtToken.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBeer.Framework.Mvc.Authentication.Jwt
+{
+   public class JwtToken
+    {
+        /// <summary>
+        /// 头部
+        /// </summary>
+        public JwtHeader Header { get; set; } = new JwtHeader();
+        /// <summary>
+        /// 载荷(内容)
+        /// </summary>
+        public JwtPayload Payload { get; set; } = new JwtPayload();
+        /// <summary>
+        /// 签名
+        /// </summary>
+        public string Signature { get; set; } = string.Empty;
+        /// <summary>
+        /// 加密密钥
+        /// </summary>
+        public string Secret { get; set; }
+    }
+}
