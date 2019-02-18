79a4ca7044d361761adc8a1b50bec6bab947ce97
吴纪雄 <549590615@qq.com>
2019-02-18 16:43:55 +0800

    整合修改

---
+++ IdentityServerSolution/Configs.cs
@@ -0,0 +1,41 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerSolution
+{
+    /// <summary>
+    /// Config是IdentityServer的配置文件,一会我们需要注册到DI层.
+    /// </summary>
+    public class Config
+    {
+        /// <summary>
+        /// 这个ApiResource参数就是我们Api
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> GetSoluction()
+        {
+            return new[]
+            {
+                new ApiResource("api1","MY API")
+            };
+        }
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId ="",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets={
+                        new Secret("secret".Sha256()),
+                    },
+                    AllowedScopes = { "api1"}
+                }
+            };
+        }
+    }
+}
---
+++ IdentityServerSolution/Controllers/HomeControllers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServerSolution
+{
+    [Route("identity")]
+    [Authorize]
+    [ApiController]
+    public class HomeControllers:ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from x in User.Claims select new { x.Type,x.Value});
+        }
+    }
+}
